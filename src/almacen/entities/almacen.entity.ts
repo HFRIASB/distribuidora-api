68f053b6c0db0d01308c1dcf831f1e8678103828
HFRIASB <friashenry0@gmail.com>
2022-09-11 22:21:20 -0400

    alamcen

---
+++ src/almacen/entities/almacen.entity.ts
@@ -0,0 +1,34 @@
+import { Producto } from "src/producto/entities/producto.entity";
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+
+export class Almacen extends BaseEntity{
+
+    @PrimaryGeneratedColumn()
+    id_almacen: number;
+
+    @Column({type: "varchar", length: 50})
+    chofer_almacen: string;
+
+    @Column()
+    fecha_almacen: Date;
+    
+    @Column({type: "varchar", length: 50})
+    tipo_almacen: string;//Producto o envase
+
+    @Column()
+    cantidad_almacen: number;
+
+    @Column({type: "varchar", length: 50})
+    item_almacen: string;
+
+    @Column()
+    estado_almacen: string;//Ingreso o despacho
+
+
+
+   
+
+   
+}
