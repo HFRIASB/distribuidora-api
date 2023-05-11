78cbc696864894d41c6e4b8d93777f3fbcb3df34
pintex <andre.pinto9723@gmail.com>
2023-05-10 23:30:18 -0400

    added filter product by vendor

--- src/cartera_cliente/cartera_cliente.controller.ts
+++ src/cartera_cliente/cartera_cliente.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { CarteraClienteService } from './cartera_cliente.service';
 import { CreateCarteraClienteDto } from './dto/create-cartera_cliente.dto';
 import { UpdateCarteraClienteDto } from './dto/update-cartera_cliente.dto';
@@ -11,6 +20,15 @@ export class CarteraClienteController {
   create(@Body() createCarteraClienteDto: CreateCarteraClienteDto) {
     return this.carteraClienteService.create(createCarteraClienteDto);
   }
+  @Get('vendedor')
+  findNumberOfOrdensByVendor() {
+    return this.carteraClienteService.findNumberOfOrdensByVendor();
+  }
+  @Get('filterDate')
+  findNumberOfOrdersByDate( @Query('fechaInicio') fechaInicio: Date,
+  @Query('fechaFin') fechaFin: Date,) {
+    return this.carteraClienteService.findNumberOfOrdersByDate(fechaInicio,fechaFin);
+  }
 
   @Get()
   find() {
@@ -22,10 +40,10 @@ export class CarteraClienteController {
     return this.carteraClienteService.findByVendedor(+id);
   }
 
-  @Get(':id')
-  findVendedorByClienteId(@Param('id') id: string) {
-    return this.carteraClienteService.findvendedorByClientId(+id);
-  }
+  // @Get(':id')
+  // findVendedorByClienteId(@Param('id') id: string) {
+  //   return this.carteraClienteService.findvendedorByClientId(+id);
+  // }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
